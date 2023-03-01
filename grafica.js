fb7d7bf532cb6198dd25a098ff1b0462165b4564
Vega Juan Carlos <juancarlosvega996@gmail.com>
2023-02-28 21:55:25 -0300

    act28feb

---
+++ grafica.js
@@ -0,0 +1,18 @@
+new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Conocimientos adquiridos al momento",
+      }
+    }
+  });
+
+  
